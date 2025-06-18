331d728a06af03708ef6a3508a7729bf502c68cf
ArielMasson <ariel.timoteo.masson@gmail.com>
2025-06-18 00:24:12 -0300

    1173 - Preenchimento de Vetor I

---
+++ 1173 - Preenchimento de Vetor I.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int N;
+    int lista[10];
+    
+    scanf("%d", &N);
+    
+    for (int c = 0; c < 10; c++){
+         lista[c] = N * pow(2, c);
+        
+        printf("N[%d] = %d\n", c, lista[c]);
+    }
+
+    return 0;
+}
