2b328d98fd94447092aec6e4c02538f1635ddf8e
ArielMasson <ariel.timoteo.masson@gmail.com>
2025-06-18 00:27:35 -0300

    Problema: 1176 - Fibonacci em Vetor.c

---
+++ Problema: 1176 - Fibonacci em Vetor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int T, N, j;
+    long long int fibonacci[61];
+
+   
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
+     
+    for(j = 2; j <= 60; j++){
+        fibonacci[j] = fibonacci[j - 1] + fibonacci[j - 2]; 
+    }
+    
+    scanf("%d", &T);
+    
+    for (int i = 0; i < T; i++){
+        scanf("%d", &N);
+        
+        printf("Fib(%d) = %lld\n", N, fibonacci[N]);
+    }   
+}
