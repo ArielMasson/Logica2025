372219247b9c03576caace05917d5d8017c37624
ArielMasson <ariel.timoteo.masson@gmail.com>
2025-06-18 19:05:19 -0300

    Problema: 1185 - Acima da Diagonal Secundária.c

---
+++ Problema: 1185 - Acima da Diagonal Secundária.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    double M[12][12];
+    int L;
+    double soma=0,  media = 0, elem;
+    char O;
+    
+    scanf(" %c", &O);
+    for(int i=0; i < 12; i++){
+        for(int j=0; j < 12; j++){
+            scanf("%lf", &M[i][j]);
+            if(i+j<11){
+                soma += M[i][j];
+            }
+        }   
+    }
+    
+    media = soma / 66; 
+    
+    if (O == 'S'){
+        printf("%.1lf\n", soma);
+    } else if (O == 'M'){
+        printf("%.1lf\n", media);
+    }
+
+    return 0;
+}
