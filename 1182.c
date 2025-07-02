7f0969d5cb8315aa1dcebcdb0e677f8e36887d66
ArielMasson <ariel.timoteo.masson@gmail.com>
2025-07-02 16:20:32 -0300

    1182.c

--- 1182.c
+++ 1182.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     double M[12][12];
     int L;
     double soma=0,  media = 0, elem;
@@ -10,23 +9,23 @@ int main()
     scanf("%d", &L);
     scanf(" %c", &T);
     
-    for(int i=0; i < 12; i++){
-        for(int j=0; j < 12; j++){
+    for(int i=0; i < 12; i++) {
+        for(int j=0; j < 12; j++) {
             scanf("%lf", &M[i][j]);
         }   
     }
     
-    for(int j=0; j < 12; j++){
+    for(int j=0; j < 12; j++) {
         soma += M[j][L];
     }
     
     media = soma / 12; 
    
-    if (T == 'S'){
+    if (T == 'S') {
         printf("%.1lf\n", soma);
-    } else if (T == 'M'){
+    } else if (T == 'M') {
         printf("%.1lf\n", media);
     }
-
+    
     return 0;
 }
