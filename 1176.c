c124b0995642d1195984d74b393b19c7e8b5569f
ArielMasson <ariel.timoteo.masson@gmail.com>
2025-07-02 15:58:21 -0300

    1176.c

--- 1176.c
+++ 1176.c
@@ -5,19 +5,17 @@ int main()
     int T, N, j;
     long long int fibonacci[61];
 
-   
     fibonacci[0] = 0;
     fibonacci[1] = 1;
      
-    for(j = 2; j <= 60; j++){
+    for(j = 2; j <= 60; j++) {
         fibonacci[j] = fibonacci[j - 1] + fibonacci[j - 2]; 
     }
     
     scanf("%d", &T);
     
-    for (int i = 0; i < T; i++){
+    for (int i = 0; i < T; i++) {
         scanf("%d", &N);
-        
         printf("Fib(%d) = %lld\n", N, fibonacci[N]);
     }   
 }
