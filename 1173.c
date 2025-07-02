57ab6f8bdc5ece4e15f399f2be04be986b939a8f
ArielMasson <ariel.timoteo.masson@gmail.com>
2025-07-02 16:10:49 -0300

    1173.c

--- 1173.c
+++ 1173.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 
-int main()
-{
+int main() {
     int N;
     int lista[10];
     
     scanf("%d", &N);
     
     for (int c = 0; c < 10; c++){
-         lista[c] = N * pow(2, c);
-        
+        lista[c] = N * pow(2, c);
         printf("N[%d] = %d\n", c, lista[c]);
     }
 
